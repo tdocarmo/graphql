7b796a0dea9ff25011abcefec4de90da5233b3e6
Tdocarmo <toni.docarmo@live.fr>
2024-11-25 17:12:19 +0100

    feat "add queries.js"

---
+++ src/graphql/queries.js
@@ -0,0 +1,103 @@
+import { gql } from '@apollo/client';
+
+export const GET_USER_PROFILE = gql`
+  query GetUserProfile($userId: ID!) {
+    user(id: $userId) {
+      id
+      name
+      email
+      profilePicture
+      skills
+      experience
+    }
+  }
+`;
+
+export const GET_USER_STATS = gql`
+  query GetUserStats($userId: ID!) {
+    userStats(id: $userId) {
+      totalProjects
+      totalExperience
+      skillRatings
+    }
+  }
+`;
+
+export const GET_USER_GROUPS = gql`
+  query GetUserGroups($userId: ID!) {
+    groups(userId: $userId) {
+      id
+      objectId
+      eventId
+      captainId
+      status
+      createdAt
+    }
+  }
+`;
+
+export const GET_GROUP_DETAILS = gql`
+  query GetGroupDetails($groupId: ID!) {
+    group(id: $groupId) {
+      id
+      objectId
+      eventId
+      captainId
+      status
+      updatedAt
+      users {
+        id
+        name
+      }
+    }
+  }
+`;
+
+export const GET_GROUP_AUDITS = gql`
+  query GetGroupAudits($groupId: ID!) {
+    audits(groupId: $groupId) {
+      id
+      grade
+      auditorId
+      attrs
+      createdAt
+      resultId
+      endAt
+      private
+    }
+  }
+`;
+
+export const GET_AUDIT_DETAILS = gql`
+  query GetAuditDetails($auditId: ID!) {
+    audit(id: $auditId) {
+      id
+      groupId
+      auditorId
+      attrs
+      grade
+      createdAt
+      updatedAt
+      code
+      resultId
+      version
+      endAt
+      private
+    }
+  }
+`;
+
+export const GET_USER_RESULT = gql`
+  query GetUserResult($userId: ID!, $objectId: ID!) {
+    result(userId: $userId, objectId: $objectId) {
+      id
+      grade
+      createdAt
+      updatedAt
+      attrs
+      version
+      eventId
+      isLast
+    }
+  }
+`;
