8e269a0d66039d8949a84cdc53d7ae783ce29952
Tdocarmo <toni.docarmo@live.fr>
2024-11-27 15:40:46 +0100

    feat "add request"

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,102 +1,44 @@
 import { gql } from "@apollo/client";
 
 export const GET_USER_PROFILE = gql`
-  query MyQuery {
+query MyQuery {
+  user {
+    id
+    login
+    attrs
+    auditRatio
+    totalUp
+    totalDown
+    xps {
+      amount
+      path
+    }
+  }
+  transaction {
+    type
+    amount
+  }
+}
+`;
+
+export const GET_LAST_PROJECT = gql`
+  query GetLastProject {
     user {
-      firstName
-      lastName
-      auditRatio
-      campus
-      createdAt
-    }
-  }
-`;
-
-export const GET_USER_STATS = gql`
-  query GetUserStats($userId: ID!) {
-    userStats(id: $userId) {
-      totalProjects
-      totalExperience
-      skillRatings
-    }
-  }
-`;
-
-export const GET_USER_GROUPS = gql`
-  query GetUserGroups($userId: ID!) {
-    groups(userId: $userId) {
-      id
-      objectId
-      eventId
-      captainId
-      status
-      createdAt
-    }
-  }
-`;
-
-export const GET_GROUP_DETAILS = gql`
-  query GetGroupDetails($groupId: ID!) {
-    group(id: $groupId) {
-      id
-      objectId
-      eventId
-      captainId
-      status
-      updatedAt
-      users {
-        id
-        name
+      xps(order_by: { amount: desc }, limit: 3) {
+        amount
+        originEventId
+        path
       }
     }
   }
 `;
 
-export const GET_GROUP_AUDITS = gql`
-  query GetGroupAudits($groupId: ID!) {
-    audits(groupId: $groupId) {
-      id
-      grade
-      auditorId
-      attrs
+export const GET_XP = gql`
+  query GetXp {
+    transaction(where: { type: { _eq: "xp" } }) {
+      amount
+      path
       createdAt
-      resultId
-      endAt
-      private
-    }
-  }
-`;
-
-export const GET_AUDIT_DETAILS = gql`
-  query GetAuditDetails($auditId: ID!) {
-    audit(id: $auditId) {
-      id
-      groupId
-      auditorId
-      attrs
-      grade
-      createdAt
-      updatedAt
-      code
-      resultId
-      version
-      endAt
-      private
-    }
-  }
-`;
-
-export const GET_USER_RESULT = gql`
-  query GetUserResult($userId: ID!, $objectId: ID!) {
-    result(userId: $userId, objectId: $objectId) {
-      id
-      grade
-      createdAt
-      updatedAt
-      attrs
-      version
-      eventId
-      isLast
     }
   }
 `;
